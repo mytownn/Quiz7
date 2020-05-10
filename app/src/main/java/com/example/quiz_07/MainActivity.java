222bd8abddf3af9453e060d72863827215844986
yeahhhhj <50025713+yeahhhhj@users.noreply.github.com>
2020-05-10 16:52:17 +0900

    yejin

--- app/src/main/java/com/example/quiz_07/MainActivity.java
+++ app/src/main/java/com/example/quiz_07/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
     }
 
     //옵션메뉴 생성
